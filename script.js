87b2dd8f5ed8fc17dad9ba928438ae3a23371d0e
jaredhorn <104010881+jaredhorn@users.noreply.github.com>
2023-07-06 09:56:48 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 var slideIndex = 1;
-showSlides(slideIndex);
+var slideTimeout;
 
 // Next/previous controls
 function plusSlides(n) {
+  clearTimeout(slideTimeout);
   showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
+  clearTimeout(slideTimeout);
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  } else if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 4000);
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slideTimeout = setTimeout(function() {
+    showSlides(slideIndex);
+  }, 4000);
 }
+
+// Call showSlides at the beginning to display the initial slide
+showSlides(slideIndex);
